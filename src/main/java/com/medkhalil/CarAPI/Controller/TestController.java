e808faf1699c1886e491718cee6e580a41b39ad3
KHALIL Mohamed <m3d.khalil01@gmail.com>
2023-04-10 13:32:19 +0200

    implémentation d'objet métier "Car"

--- src/main/java/com/medkhalil/CarAPI/Controller/TestController.java
+++ src/main/java/com/medkhalil/CarAPI/Controller/TestController.java
@@ -1,9 +1,13 @@
-package com.medkhalil.CarAPI.Controller;
+package com.medkhalil.CarAPI.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     
-
+    @GetMapping("/")
+    public String getHome(){
+        return "Home";
+    }
 }
